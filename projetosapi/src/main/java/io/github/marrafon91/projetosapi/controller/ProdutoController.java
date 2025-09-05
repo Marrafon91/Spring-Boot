641eab0cf6e6458d503a553b8b8397189e3c8526
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-09-05 17:17:25 -0300

    Metodo PUT para atualizar dados.

--- projetosapi/src/main/java/io/github/marrafon91/projetosapi/controller/ProdutoController.java
+++ projetosapi/src/main/java/io/github/marrafon91/projetosapi/controller/ProdutoController.java
@@ -39,4 +39,12 @@ public class ProdutoController {
     public void deletar(@PathVariable("id") String id) {
         produtoRepository.deleteById(id);
     }
+
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable("id") String id,
+                          @RequestBody Produto produto) {
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+
 }
