7d197ed85ef1ba34db040917f291c62a9881384b
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-09-04 21:09:23 -0300

    Requisicao POST - mapeamento

---
+++ projetosapi/src/main/java/io/github/marrafon91/projetosapi/model/Produto.java
@@ -0,0 +1,74 @@
+package io.github.marrafon91.projetosapi.model;
+
+import java.util.Objects;
+
+public class Produto {
+    private String id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+
+    public Produto() {}
+
+    public Produto(String id, String nome, String descricao, Double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return Objects.equals(id, produto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+}
