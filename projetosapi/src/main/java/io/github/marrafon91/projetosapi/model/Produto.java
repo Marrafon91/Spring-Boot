ffec1022462bdb3cc4bf7694f710ecd01570a8bf
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-09-05 07:28:15 -0300

    Mapeamento da classe Produto

--- projetosapi/src/main/java/io/github/marrafon91/projetosapi/model/Produto.java
+++ projetosapi/src/main/java/io/github/marrafon91/projetosapi/model/Produto.java
@@ -2,10 +2,26 @@ package io.github.marrafon91.projetosapi.model;
 
 import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produto")
 public class Produto {
+
+    @Id
+    @Column(name = "id")
     private String id;
+
+    @Column(name = "nome")
     private String nome;
+
+    @Column(name = "descricao")
     private String descricao;
+
+    @Column(name = "preco")
     private Double preco;
 
     public Produto() {}
