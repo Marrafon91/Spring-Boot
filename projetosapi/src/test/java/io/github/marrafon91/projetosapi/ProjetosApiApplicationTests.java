87eb906339f6d063741d77c9c51b0ba2870cfe92
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-09-05 07:16:47 -0300

    Crianco conexao com banco H2, criando Tabela no data.sql

--- projetosapi/src/test/java/io/github/marrafon91/projetosapi/ProjetosApiApplicationTests.java
+++ projetosapi/src/test/java/io/github/marrafon91/projetosapi/ProjetosApiApplicationTests.java
@@ -3,6 +3,7 @@ package io.github.marrafon91.projetosapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class ProjetosApiApplicationTests {
 
