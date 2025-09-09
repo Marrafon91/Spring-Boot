973a31a0941f3a29b5ddccedf9ebe75ef8f849eb
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-09-09 16:12:00 -0300

    Aprendendo sobre Scope

--- arquiteturaspring/src/main/java/io/github/marrafon91/arquiteturaspring/BeanGerenciado.java
+++ arquiteturaspring/src/main/java/io/github/marrafon91/arquiteturaspring/BeanGerenciado.java
@@ -3,9 +3,17 @@ package io.github.marrafon91.arquiteturaspring;
 import io.github.marrafon91.arquiteturaspring.todos.TodoEntity;
 import io.github.marrafon91.arquiteturaspring.todos.TodoValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
 
 @Component
+@Scope(BeanDefinition.SCOPE_SINGLETON)
+//@Scope(WebApplicationContext.SCOPE_APPLICATION)
+//@Scope("request")
+//@Scope("session")
+//@Scope("application")
 public class BeanGerenciado {
 
     @Autowired
