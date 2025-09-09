5349146e66c37327e4cee5f3971641110ad8d0d8
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-09-09 16:38:03 -0300

    Lazy dos Beans

--- arquiteturaspring/src/main/java/io/github/marrafon91/arquiteturaspring/Application.java
+++ arquiteturaspring/src/main/java/io/github/marrafon91/arquiteturaspring/Application.java
@@ -15,6 +15,7 @@ public class Application {
 		SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
 		builder.bannerMode(Banner.Mode.OFF);
 		builder.profiles("producao");
+//		builder.lazyInitialization(true);
 
 		
 		builder.run(args);
