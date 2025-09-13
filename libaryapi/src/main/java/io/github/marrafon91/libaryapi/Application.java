db8a27e52c54b44e14bdd8cda0b9a20012b920e3
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-09-13 18:19:52 -0300

    Applicando testes no repository

--- libaryapi/src/main/java/io/github/marrafon91/libaryapi/Application.java
+++ libaryapi/src/main/java/io/github/marrafon91/libaryapi/Application.java
@@ -1,28 +1,12 @@
 package io.github.marrafon91.libaryapi;
 
-import io.github.marrafon91.libaryapi.model.Autor;
-import io.github.marrafon91.libaryapi.repository.AutorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		var context = SpringApplication.run(Application.class, args);
-		AutorRepository repository = context.getBean(AutorRepository.class);
+		SpringApplication.run(Application.class, args);
 
-		exemploSalvarResgistro(repository);
-	}
-
-	public static void exemploSalvarResgistro(AutorRepository autorRepository) {
-		Autor autor = new Autor();
-		autor.setName("Jose");
-		autor.setNacionalidade("Brasileiro");
-		autor.setDataNascimento(LocalDate.of(1950, 1, 31));
-
-		var autorSalvo = autorRepository.save(autor);
-		System.out.println("Autor Salvo: " + autorSalvo);
 	}
 }
---
+++ libaryapi/src/test/java/io/github/marrafon91/libaryapi/repository/AutorRepositoryTest.java
@@ -0,0 +1,48 @@
+package io.github.marrafon91.libaryapi.repository;
+
+import io.github.marrafon91.libaryapi.model.Autor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.UUID;
+
+@SpringBootTest
+public class AutorRepositoryTest {
+
+    @Autowired
+    AutorRepository repository;
+
+    @Test
+    public void salvarTest() {
+        Autor autor = new Autor();
+        autor.setName("Maria");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of(1951, 7, 19));
+
+
+        var autorSalvo = repository.save(autor);
+        System.out.println("Autor Salvo: " + autorSalvo);
+
+    }
+
+    @Test
+    public void atualizarTest() {
+       var id = UUID.fromString("0e717e1a-b62a-4e4c-94c1-720cdd3ad221");
+
+       Optional<Autor> possivelAutor = repository.findById(id);
+
+       if (possivelAutor.isPresent()) {
+
+           Autor autorEncontrado = possivelAutor.get();
+           System.out.println("Dados do Autor:");
+           System.out.println(autorEncontrado);
+
+           autorEncontrado.setDataNascimento(LocalDate.of(1960, 11, 7));
+
+           repository.save(autorEncontrado);
+       }
+    }
+}
