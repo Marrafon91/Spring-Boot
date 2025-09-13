82053009a128f3a6ab00bc309d49ebfd9db02ce3
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-09-13 11:22:44 -0300

    Criei a Entidade Autor

---
+++ libaryapi/src/main/java/io/github/marrafon91/libaryapi/model/Autor.java
@@ -0,0 +1,35 @@
+package io.github.marrafon91.libaryapi.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "autor", schema = "public")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Autor {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade", length = 50, nullable = false)
+    private String nacionalidade;
+
+}
