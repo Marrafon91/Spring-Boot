b0bf149d75c5bc1af828a47138d301577fbd199d
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-09-15 15:25:38 -0300

    Mais sobre Query Methods

--- libaryapi/src/main/java/io/github/marrafon91/libaryapi/repository/LivroRepository.java
+++ libaryapi/src/main/java/io/github/marrafon91/libaryapi/repository/LivroRepository.java
@@ -5,11 +5,14 @@ import io.github.marrafon91.libaryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 public interface LivroRepository extends JpaRepository<Livro, UUID> {
 
+    //https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html
+    
     //@Querry Method
     //select * from livro where id_autor = id
     List<Livro> findByAutor(Autor autor);
@@ -22,7 +25,10 @@ public interface LivroRepository extends JpaRepository<Livro, UUID> {
 
     //select * from livro where titulo = ? and preco = ?
     List<Livro> findByTituloAndPreco(String titulo, BigDecimal preco);
-    
+
     //select * from livro where titulo = ? or isbn = ?
     List<Livro> findByTituloOrIsbn(String titulo, String isbn);
+
+    //select * from livro where data_publicacao between ? and ?
+    List<Livro> findByDataPublicacaoBetween(LocalDate inicio, LocalDate fim);
 }
