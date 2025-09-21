c74c715ab4523d6bd56c6a3f569007c472c37137
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-09-21 09:46:50 -0300

    Validacao Spring

--- libaryapi/src/main/java/io/github/marrafon91/libaryapi/controller/dto/AutorDTO.java
+++ libaryapi/src/main/java/io/github/marrafon91/libaryapi/controller/dto/AutorDTO.java
@@ -3,6 +3,8 @@ package io.github.marrafon91.libaryapi.controller.dto;
 import io.github.marrafon91.libaryapi.model.Autor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -10,9 +12,12 @@ import java.util.UUID;
 public record AutorDTO(
         UUID id,
         @NotBlank(message = "Campo obrigatorio")
+        @Size(min = 2, max = 100, message = "Campo fora do tamanho padrao")
         String nome,
         @NotNull(message = "Campo obrigatorio")
+        @Past(message = "Não pode ser uma data futura")
         LocalDate dataNascimento,
+        @Size(min = 2,max = 50, message = "Campo fora do tamanho padrao")
         @NotBlank(message = "Campo obrigatorio")
         String nacionalidade) {
 
