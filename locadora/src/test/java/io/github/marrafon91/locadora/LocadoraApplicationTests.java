3fa7536256a38d2a4df590d64088e13c2b050c1d
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-11-07 20:31:17 -0300

    Criando o projeto locadora para estudos de TESTESgit status!

---
+++ locadora/src/test/java/io/github/marrafon91/locadora/LocadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.marrafon91.locadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
