bb37a5b18f0fe8340dd3369fe35422f82e171b09
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-11-10 08:19:03 -0300

    Testando Exceptions e finalizando o desafio

--- locadora/src/test/java/io/github/marrafon91/locadora/model/ReservaTest.java
+++ locadora/src/test/java/io/github/marrafon91/locadora/model/ReservaTest.java
@@ -1,13 +1,14 @@
 package io.github.marrafon91.locadora.model;
 
 import io.github.marrafon91.locadora.model.exceptions.ReservaInvalidaException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ReservaTest {
 
@@ -36,17 +37,31 @@ class ReservaTest {
     @Test
     @DisplayName("Test da reserva com dias inválidos")
     public void reservaDiasInvalidos() {
-        Carro carro = new Carro("Sedan", 100.00);
-
-        Assertions.assertThrows(ReservaInvalidaException.class, () -> {
-            carro.calcularValorAluguel(0);
+        //JUnit
+        assertThrows(ReservaInvalidaException.class, () -> {
+            new Reserva(carro, cliente, 0);
         });
+
+        assertDoesNotThrow(() -> {
+            new Reserva(carro, cliente, 1);
+        });
+
+        //AsserJ
+        var erro = catchThrowable(() -> new Reserva(carro, cliente, 0));
+
+        assertThat(erro)
+                .isInstanceOf(ReservaInvalidaException.class)
+                .hasMessage("Quantidade de dias inválida");
     }
 
     @Test
-    @DisplayName("")
+    @DisplayName("Calculando o valor total do Aluguel")
     public void totalAluguel() {
+        var reserva = new Reserva(carro, cliente, 3);
 
+        var total = reserva.calcularTotal();
+
+        assertThat(total).isEqualTo(300);
     }
 
 }
