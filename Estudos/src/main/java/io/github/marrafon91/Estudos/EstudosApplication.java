fa20bd200c7d3713a3166b303b85ba0122976513
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-11-18 06:53:38 -0300

    Implementando o projeto no Spring Boot

--- Estudos/src/main/java/io/github/marrafon91/Estudos/EstudosApplication.java
+++ Estudos/src/main/java/io/github/marrafon91/Estudos/EstudosApplication.java
@@ -1,13 +1,25 @@
 package io.github.marrafon91.Estudos;
 
+import io.github.marrafon91.Estudos.entities.Employee;
+import io.github.marrafon91.Estudos.services.SalaryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EstudosApplication {
+public class EstudosApplication implements CommandLineRunner {
+
+	@Autowired
+	private SalaryService salaryService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudosApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Employee employee = new Employee("Maria", 4000);
+		System.out.println(salaryService.netSalary(employee));
+	}
 }
---
+++ Estudos/src/main/java/io/github/marrafon91/Estudos/services/PensionService.java
@@ -0,0 +1,11 @@
+package io.github.marrafon91.Estudos.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PensionService {
+
+    public double discount(double amount) {
+        return amount * 0.1;
+    }
+}
