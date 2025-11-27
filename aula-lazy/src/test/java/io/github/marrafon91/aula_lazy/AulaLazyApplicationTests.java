240f0858f6a333f33e934161063e7e1feb06fcda
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-11-27 19:03:00 -0300

    Aula Lazy

---
+++ aula-lazy/src/test/java/io/github/marrafon91/aula_lazy/AulaLazyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.marrafon91.aula_lazy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaLazyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
