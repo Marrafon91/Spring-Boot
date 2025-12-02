d8c37f041a650425f4643b84ea9de6868441dd2b
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-12-02 11:44:31 -0300

    URI 2602 Spring Boot JPQL 7m

--- uri/uri2602/src/main/java/io/github/marrafon91/uri/UriApplication2602.java
+++ uri/uri2602/src/main/java/io/github/marrafon91/uri/UriApplication2602.java
@@ -23,11 +23,21 @@ public class UriApplication2602 implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		List<CustomerMinProjection> list = repository.search1("RS");
+		List<CustomerMinProjection> list = repository.search1("rs");
 		List<CustomerMinDTO> result1 = list.stream().map(CustomerMinDTO::new).toList();
 
+		System.out.println("Resultado SQL RAIZ:");
 		for (CustomerMinDTO obj : result1) {
 			System.out.println(obj);
 		}
+		System.out.println("\n\n");
+
+		List<CustomerMinDTO> result2 = repository.search2("rs");
+
+		System.out.println("Resultado JPQL");
+		for (CustomerMinDTO obj : result2) {
+			System.out.println(obj);
+		}
+
 	}
 }
--- uri/uri2602/src/main/java/io/github/marrafon91/uri/repositories/CustomerRepository.java
+++ uri/uri2602/src/main/java/io/github/marrafon91/uri/repositories/CustomerRepository.java
@@ -1,5 +1,6 @@
 package io.github.marrafon91.uri.repositories;
 
+import io.github.marrafon91.uri.dto.CustomerMinDTO;
 import io.github.marrafon91.uri.entities.Customer;
 import io.github.marrafon91.uri.projections.CustomerMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +12,11 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     @Query(nativeQuery = true, value = "SELECT name "
             + "FROM customers "
-            + "WHERE state= :state ")
+            + "WHERE UPPER(state) = UPPER(:state) ")
     List<CustomerMinProjection> search1(String state);
+
+    @Query("SELECT new io.github.marrafon91.uri.dto.CustomerMinDTO(obj.name) "
+            + "FROM Customer obj "
+            + "WHERE UPPER(obj.state) = UPPER(:state) ")
+    List<CustomerMinDTO> search2(String state);
 }
