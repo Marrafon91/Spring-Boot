6a877a995d98d6e99ce05be20455e196e6060807
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-12-04 16:34:59 -0300

    uri2619

---
+++ uri/uri2619/src/test/java/io/github/marrafon91/uri/UriApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.marrafon91.uri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
