c4222afe83ab56e4ea33a250d1f737c1518f5cbc
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-12-04 20:52:45 -0300

    ure2622

--- uri/uri2622/2622.sql
+++ uri/uri2622/2622.sql
@@ -1,7 +1,7 @@
 --- URI Online Judge SQL
 --- Copyright URI Online Judge
 --- www.urionlinejudge.com.br
---- Problem 2620
+--- Problem 2622
 
 CREATE TABLE customers (
   id numeric PRIMARY KEY,
@@ -12,38 +12,32 @@ CREATE TABLE customers (
   credit_limit numeric
 );
 
-CREATE TABLE orders (
-  id numeric PRIMARY KEY,
-  orders_date date,
-  id_customers numeric REFERENCES customers (id)
+CREATE TABLE legal_person (
+  id_customers numeric REFERENCES customers (id),
+  cnpj char (18),
+  contact varchar(255)
 );
-
 
 INSERT INTO customers (id, name, street, city, state, credit_limit)
 VALUES
   (1,	'Nicolas Diogo Cardoso',	'Acesso Um',	'Porto Alegre',	'RS',	475),
   (2,	'Cecília Olivia Rodrigues',	'Rua Sizuka Usuy',	'Cianorte',	'PR',	3170),
   (3,	'Augusto Fernando Carlos Eduardo Cardoso',	'Rua Baldomiro Koerich',	'Palhoça',	'SC',	1067),
-  (4,	'Pedro Cardoso',	'Acesso Um',	'Porto Alegre',	'RS',	475),
+  (4,	'Nicolas Diogo Cardoso',	'Acesso Um',	'Porto Alegre',	'RS',	475),
   (5,	'Sabrina Heloisa Gabriela Barros',	'Rua Engenheiro Tito Marques Fernandes',	'Porto Alegre',	'RS',	4312),
   (6,	'Joaquim Diego Lorenzo Araújo',	'Rua Vitorino',	'Novo Hamburgo',	'RS',	2314);
-
-INSERT INTO orders (id, orders_date, id_customers)
+  
+INSERT INTO legal_person (id_customers, cnpj, contact)
 VALUES
-  (1,	'2016-05-13',	3),
-  (2,	'2016-01-12',	2),
-  (3,	'2016-04-18',	5),
-  (4,	'2016-09-07',	4),
-  (5,	'2016-02-13',	6),
-  (6,	'2016-08-05',	3);
+  (4,	'85883842000191',	'99767-0562'),
+  (5,	'47773848000117',	'99100-8965');
+  
 
+/*  Execute this query to drop the tables */
+-- DROP TABLE legal_person, customers; --
 
-  /*  Execute this query to drop the tables */
-  -- DROP TABLE orders, customers; --
-
-  -- RESPOSTA
-  SELECT customers.name, orders.id
-  FROM orders
-  INNER JOIN customers ON customers.id = id_customers
-  WHERE orders.orders_date
-  BETWEEN '2016-01-01' AND '2016-06-30'
+-- RESPOSTA
+SELECT customers.name
+FROM legal_person
+INNER JOIN customers ON customers.id = id_customers
+WHERE legal_person.cnpj IS NOT NULL;
---
+++ uri/uri2622/src/main/java/io/github/marrafon91/uri/UriApplication2622.java
@@ -0,0 +1,19 @@
+package io.github.marrafon91.uri;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UriApplication2622 implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UriApplication2622.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+
+	}
+}
