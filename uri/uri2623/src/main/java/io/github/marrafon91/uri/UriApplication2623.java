bf5b26c5466831102f3c34a70460fe02836d2f31
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-12-04 21:11:33 -0300

    uri2623

---
+++ uri/uri2623/src/main/java/io/github/marrafon91/uri/UriApplication2623.java
@@ -0,0 +1,19 @@
+package io.github.marrafon91.uri;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UriApplication2623 implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UriApplication2623.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+
+	}
+}
