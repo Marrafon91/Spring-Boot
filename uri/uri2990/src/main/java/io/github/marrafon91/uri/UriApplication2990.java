5a551be9cdaa9e61ca9cba0a09d78690796e4615
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-12-08 12:49:05 -0300

    URI 2990 Spring Boot SQL e JPQL

--- uri/uri2990/src/main/java/io/github/marrafon91/uri/UriApplication2990.java
+++ uri/uri2990/src/main/java/io/github/marrafon91/uri/UriApplication2990.java
@@ -1,11 +1,20 @@
 package io.github.marrafon91.uri;
 
+import io.github.marrafon91.uri.dto.EmpregadoDepDTO;
+import io.github.marrafon91.uri.projection.EmpregadoDeptProjection;
+import io.github.marrafon91.uri.repository.EmpregadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class UriApplication2990 implements CommandLineRunner {
+
+	@Autowired
+	private EmpregadoRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UriApplication2990.class, args);
@@ -14,6 +23,33 @@ public class UriApplication2990 implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		List<EmpregadoDeptProjection> list1 = repository.search1();
+		List<EmpregadoDepDTO> result1 = list1.stream().map(EmpregadoDepDTO::new).toList();
 
+		System.out.println("\n");
+		System.out.println("*** RESULTADO SQL RAIZ NOT IN: \n");
+		for (EmpregadoDepDTO obj : result1) {
+			System.out.println(obj);
+		}
+		System.out.println("\n");
+
+		List<EmpregadoDepDTO> result2 = repository.search2();
+
+		System.out.println("\n");
+		System.out.println("*** RESULTADO JPQL: \n");
+		for (EmpregadoDepDTO obj : result2) {
+			System.out.println(obj);
+		}
+		System.out.println("\n");
+
+		List<EmpregadoDeptProjection> list3 = repository.search3();
+		List<EmpregadoDepDTO> result3 = list3.stream().map(EmpregadoDepDTO::new).toList();
+
+		System.out.println("\n");
+		System.out.println("*** RESULTADO SQL RAIZ LEFT JOIN: \n");
+		for (EmpregadoDepDTO obj : result3) {
+			System.out.println(obj);
+		}
+		System.out.println("\n");
 	}
 }
--- uri/uri2990/src/main/java/io/github/marrafon91/uri/repository/EmpregadoRepository.java
+++ uri/uri2990/src/main/java/io/github/marrafon91/uri/repository/EmpregadoRepository.java
@@ -1,7 +1,47 @@
 package io.github.marrafon91.uri.repository;
 
+import io.github.marrafon91.uri.dto.EmpregadoDepDTO;
 import io.github.marrafon91.uri.entities.Empregado;
+import io.github.marrafon91.uri.projection.EmpregadoDeptProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmpregadoRepository extends JpaRepository<Empregado, String> {
+
+    @Query(nativeQuery = true, value = """
+            SELECT empregados.cpf, empregados.enome, departamentos.dnome
+            FROM empregados
+            INNER JOIN departamentos ON empregados.dnumero = departamentos.dnumero
+            WHERE empregados.cpf NOT IN (
+            	SELECT empregados.cpf
+            	FROM empregados
+            	INNER JOIN trabalha ON trabalha.cpf_emp = empregados.cpf
+            )
+            ORDER BY empregados.cpf;
+            """)
+    List<EmpregadoDeptProjection> search1();
+
+    @Query(value = """
+            SELECT new io.github.marrafon91.uri.dto.EmpregadoDepDTO(obj.cpf, obj.enome, obj.departamento.dnome)
+            FROM Empregado obj
+            WHERE obj.cpf NOT IN (
+            	SELECT obj.cpf
+            	FROM Empregado obj
+            	INNER JOIN obj.projetosOndeTrabalha
+            )
+            ORDER BY obj.cpf
+            """)
+    List<EmpregadoDepDTO> search2();
+
+    @Query(nativeQuery = true, value = """            
+            SELECT empregados.cpf, empregados.enome, departamentos.dnome
+            FROM empregados
+            INNER JOIN departamentos ON empregados.dnumero = departamentos.dnumero
+            LEFT JOIN trabalha ON trabalha.cpf_emp = empregados.cpf
+            WHERE trabalha.cpf_emp IS NULL
+            ORDER BY empregados.cpf;
+            """)
+    List<EmpregadoDeptProjection> search3();
 }
