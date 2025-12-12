047193b98b98e1bd48ea34ef7b716b55c05da43e
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-12-12 06:10:50 -0300

    Projeto criado

---
+++ login_acesso/src/main/java/io/github/marrfon91/login_acesso/LoginAcessoApplication.java
@@ -0,0 +1,13 @@
+package io.github.marrfon91.login_acesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAcessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAcessoApplication.class, args);
+	}
+
+}
---
+++ login_acesso/src/test/java/io/github/marrfon91/login_acesso/LoginAcessoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.marrfon91.login_acesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAcessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
