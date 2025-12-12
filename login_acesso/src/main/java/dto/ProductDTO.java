ba1fb88653123bbf4d695f12031b2a48eceff45d
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-12-12 09:38:22 -0300

    ProductDTO criado

---
+++ login_acesso/src/main/java/dto/ProductDTO.java
@@ -0,0 +1,16 @@
+package dto;
+
+import io.github.marrfon91.login_acesso.entities.Product;
+
+public record ProductDTO(
+        Long id,
+        String name
+) {
+
+    public ProductDTO(Product entity){
+        this (
+                entity.getId(),
+                entity.getName()
+        );
+    }
+}
