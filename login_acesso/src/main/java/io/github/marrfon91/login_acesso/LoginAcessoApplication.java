b3359c2626b2e6c3c019e6b5965acf96c36f691f
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-12-12 12:10:42 -0300

    BCrypt password encoder

--- login_acesso/src/main/java/io/github/marrfon91/login_acesso/LoginAcessoApplication.java
+++ login_acesso/src/main/java/io/github/marrfon91/login_acesso/LoginAcessoApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoginAcessoApplication {
+public class LoginAcessoApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginAcessoApplication.class, args);
 	}
-
-}
+	}
