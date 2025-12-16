70e0245d852f50e699bb15273fcb3011658f7a13
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-12-16 15:58:51 -0300

    Implementando o checklist do Spring Security PARTE 2/not tested

--- login_acesso/src/main/java/io/github/marrfon91/login_acesso/services/UserService.java
+++ login_acesso/src/main/java/io/github/marrfon91/login_acesso/services/UserService.java
@@ -1,12 +1,16 @@
 package io.github.marrfon91.login_acesso.services;
 
+import io.github.marrfon91.login_acesso.entities.Role;
 import io.github.marrfon91.login_acesso.entities.User;
+import io.github.marrfon91.login_acesso.projections.UserDetailsProjection;
 import io.github.marrfon91.login_acesso.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -16,9 +20,16 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = repository.findByEmail(username);
-        if (user == null) {
+        List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+        if (result.isEmpty()) {
             throw  new UsernameNotFoundException("User not found");
+        }
+
+        User user = new User();
+        user.setEmail(username);
+        user.setPassword(result.getFirst().getPassWord());
+        for (UserDetailsProjection projection : result) {
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
         }
         return user;
     }
